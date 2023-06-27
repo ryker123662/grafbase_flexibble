c5707e1b9a97a60f2c71ddd4d38ea43c99886193
ryker123662 <adamtomasz96@gmail.com>
2023-06-27 13:41:41 +0200

    nb

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,8 +1,7 @@
 import { g, auth, config } from "@grafbase/sdk";
 
-// @ts-ignore
 const User = g.model("User", {
-    name: g.string().length({ min: 2, max: 100 }),
+    name: g.string().length({ min: 2, max: 20 }),
     email: g.string().unique(),
     avatarUrl: g.url(),
     description: g.string().length({ min: 2, max: 1000 }).optional(),
@@ -14,7 +13,6 @@ const User = g.model("User", {
         .optional(),
 });
 
-// @ts-ignore
 const Project = g.model("Project", {
     title: g.string().length({ min: 3 }),
     description: g.string(),
