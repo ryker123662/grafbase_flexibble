ed9886247cc56b5b73d78ec947271a1ac8d71383
ryker123662 <adamtomasz96@gmail.com>
2023-07-04 09:50:41 +0200

    m

--- lib/actions.ts
+++ lib/actions.ts
@@ -19,7 +19,7 @@ const apiUrl = isProduction
 
 const apiKey = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || "" : "letmein";
 
-const serverUrl = isProduction ? process.env.NEXTAUTH_URL : "http://localhost:3000";
+const serverUrl = isProduction ? process.env.NEXT_PUBLIC_SERVER_URL : "http://localhost:3000";
 
 const client = new GraphQLClient(apiUrl);
 
